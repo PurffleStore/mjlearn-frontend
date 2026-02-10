df5c0bb48c1e3279c7ef052de100f334bb3a9c62
Oviya <oviya.r@pykara.net>
2026-02-10 14:20:14 +0530

    fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://mjlearn-backend-production.up.railway.app',
+  appName: 'MJ-Learn',
+  version: '1.0.0'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://mjlearn-backend-production.up.railway.app',
+  appName: 'Mj-Learn',
+  version: '1.0.0'
+};
